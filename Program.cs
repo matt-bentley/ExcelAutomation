898fb748b1c12325c6d02934d8edb7ff5c96302d
Bentley, Matt (UK - London) <matthew_j_bentley@hotmail.com>
2018-01-06 09:40:43 +0000

    Initial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EpPlusDemo.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+
+            List<string> values = new List<string>();
+
+            byte[] file = File.ReadAllBytes(@".\Source\Template.xlsx");
+            using (MemoryStream ms = new MemoryStream(file))
+            {
+                using (ExcelPackage package = new ExcelPackage(ms))
+                {
+                    if (package.Workbook.Worksheets.Count == 0)
+                        Console.WriteLine("Workbook contains no sheets");
+                    else
+                    {
+                        foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
+                        {
+                            int row = worksheet.Names["start"].Start.Row;
+                            int col = worksheet.Names["start"].Start.Column;
+                            object val = worksheet.Cells[row, col].Value;
+                            while(val != null)
+                            {
+                                values.Add(val.ToString());
+                                row++;
+                                val = worksheet.Cells[row, col].Value;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
